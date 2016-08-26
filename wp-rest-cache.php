ee8b431c56790a3fb59fbf016a185def1a1ac9e9
Jeremy Tweddle <jeremy.tweddle@interadcorp.com>
2016-08-26 17:06:45 -0400

    Removed external custom logging function call

--- wp-rest-cache.php
+++ wp-rest-cache.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP REST Cache
  * Description: This plugin enables transient based caching for WP-API v2 utilising the WP-TLC-Transients library
  * Plugin URI: https://github.com/breams/wp-rest-cache/
- * Version: 0.1.0
+ * Version: 0.1.1
  * Author: <a href="https://github.com/Shelob9/">Josh Pollock</a>, <a href="http://markjaquith.com">Mark Jaquith</a>, Jeremy Tweddle
  */
 
@@ -63,8 +63,6 @@ if ( ! function_exists( 'wp_rest_cache_get' ) ) :
 		$method = $request->get_method();
 		$request_uri = $_SERVER[ 'REQUEST_URI' ];
 
-		rbc_wp_log( esc_url($request_uri) );
-
 		/**
 		 * Cache override.
 		 *
@@ -80,11 +78,13 @@ if ( ! function_exists( 'wp_rest_cache_get' ) ) :
 		$skip_cache = apply_filters( 'wp_rest_cache_skip_cache', false, $endpoint, $method );
 		if ( $skip_cache )  {
 			$server->send_header( 'X-WP-API-Cache', 'skipped' );
+			error_log( 'skipped' );
 			return $result;
 		}
 
 		if ( $request->get_param('refresh-cache') === true ){
 			$server->send_header( 'X-WP-API-Cache', 'refreshed' );
+			error_log( 'refreshed' );
 			return $result;
 		}
 
@@ -103,14 +103,13 @@ if ( ! function_exists( 'wp_rest_cache_get' ) ) :
 
 		$cache_time = apply_filters( 'wp_rest_cache_cache_time', WP_REST_CACHE_DEFAULT_CACHE_TIME, $endpoint, $method );
 
-		rbc_wp_log( $cache_time );
-
 		$result =  tlc_transient( __FUNCTION__ . $request_uri )
 			->updates_with( 'wp_rest_cache_rebuild', array( $server, $request ) )
 			->expires_in( $cache_time )
 			->get();
 
-		$result->header( 'X-WP-API-Cache', 'cached' );
+		$result->header( 'X-WP-API-Cache', 'cached', false );
+		error_log( 'cached' );
 
 		return $result;
 	}
